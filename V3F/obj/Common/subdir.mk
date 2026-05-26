bf29f10151453f47a128ea0cf52f91a4c98ff285
AragronLi <LZY13632047419@163.com>
2026-05-27 05:56:31 +0800

    change

--- V3F/obj/Common/subdir.mk
+++ V3F/obj/Common/subdir.mk
@@ -5,14 +5,17 @@
 
 # Add inputs and outputs from these tool invocations to the build variables 
 C_SRCS += \
+d:/CH32H417/EVT/EXAM/USART/USART_Interrupt/Common/atk_ms901m_uart.c \
 d:/CH32H417/EVT/EXAM/USART/USART_Interrupt/Common/hardware.c \
 d:/CH32H417/EVT/EXAM/USART/USART_Interrupt/Common/ipc_comm.c 
 
 C_DEPS += \
+./Common/atk_ms901m_uart.d \
 ./Common/hardware.d \
 ./Common/ipc_comm.d 
 
 OBJS += \
+./Common/atk_ms901m_uart.o \
 ./Common/hardware.o \
 ./Common/ipc_comm.o 
 
@@ -27,6 +30,8 @@ DIR_EXPANDS += \
 
 
 # Each subdirectory must supply rules for building sources it contributes
+Common/atk_ms901m_uart.o: d:/CH32H417/EVT/EXAM/USART/USART_Interrupt/Common/atk_ms901m_uart.c
+	@	riscv-wch-elf-gcc -march=rv32imac_zba_zbb_zbc_zbs_xw -mabi=ilp32 -msmall-data-limit=8 -msave-restore -fmax-errors=20 -Og -fmessage-length=0 -fsigned-char -ffunction-sections -fdata-sections -fno-common -Wunused -Wuninitialized -g -DCore_V3F -I"d:/CH32H417/EVT/EXAM/SRC/Debug" -I"d:/CH32H417/EVT/EXAM/SRC/Core" -I"d:/CH32H417/EVT/EXAM/USART/USART_Interrupt/V3F/User" -I"d:/CH32H417/EVT/EXAM/SRC/Peripheral/inc" -I"d:/CH32H417/EVT/EXAM/USART/USART_Interrupt/Common" -I"d:/CH32H417/EVT/EXAM/USART/USART_Interrupt/V3F/Driver" -std=gnu99 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -c -o "$@" "$<"
 Common/hardware.o: d:/CH32H417/EVT/EXAM/USART/USART_Interrupt/Common/hardware.c
 	@	riscv-wch-elf-gcc -march=rv32imac_zba_zbb_zbc_zbs_xw -mabi=ilp32 -msmall-data-limit=8 -msave-restore -fmax-errors=20 -Og -fmessage-length=0 -fsigned-char -ffunction-sections -fdata-sections -fno-common -Wunused -Wuninitialized -g -DCore_V3F -I"d:/CH32H417/EVT/EXAM/SRC/Debug" -I"d:/CH32H417/EVT/EXAM/SRC/Core" -I"d:/CH32H417/EVT/EXAM/USART/USART_Interrupt/V3F/User" -I"d:/CH32H417/EVT/EXAM/SRC/Peripheral/inc" -I"d:/CH32H417/EVT/EXAM/USART/USART_Interrupt/Common" -I"d:/CH32H417/EVT/EXAM/USART/USART_Interrupt/V3F/Driver" -std=gnu99 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -c -o "$@" "$<"
 Common/ipc_comm.o: d:/CH32H417/EVT/EXAM/USART/USART_Interrupt/Common/ipc_comm.c
---
+++ V5F/obj/Common/atk_ms901m_uart.d
@@ -0,0 +1,2 @@
+Common/atk_ms901m_uart.o: \
+ d:/CH32H417/EVT/EXAM/USART/USART_Interrupt/Common/atk_ms901m_uart.c
